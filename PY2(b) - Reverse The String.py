1447e0843676cd743eeb93022ceb989fd5f8fc3c
devcabbage69 <prabhakaran.selvamc@gmail.com>
2024-09-11 18:59:04 +0530

    Updated PY2(b) - Reverse The String.py

--- PY2(b) - Reverse The String.py
+++ PY2(b) - Reverse The String.py
@@ -2,6 +2,6 @@ def reverse(str1):
     str2 = ''
     for i in str1:
         str2 += i
-    return str2
+        return str2
 word = input("Enter a String: ")
 print("The Reverse of The String is: ", reverse(word))
